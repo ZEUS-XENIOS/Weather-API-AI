73d160b3f11aa9db2340edcc2f098a77f0a8af2e
ZEUS-XENIOS <kapilabhinav20@gmail.com>
2026-03-15 16:34:20 +0530

    dashboard timestamp fix

--- weather_dashboard.py
+++ weather_dashboard.py
@@ -4,23 +4,22 @@ import numpy as np
 from sklearn.linear_model import LinearRegression
 
 st.title("Weather Prediction Dashboard")
+
 df = pd.read_csv("weather_history.csv")
 
-# convert date column properly
-df["date"] = pd.to_datetime(df["date"])
+# convert timestamp column properly
+df["timestamp"] = pd.to_datetime(df["timestamp"])
 
 # sort by time
-df = df.sort_values("date")
+df = df.sort_values("timestamp")
 
 # set time as index for graph
-df = df.set_index("date")
+df = df.set_index("timestamp")
 
-names=(["date","city","temperature","humidity","condition"])
-
-st.subheader(" Historical Data")
+st.subheader("Historical Data")
 st.dataframe(df)
 
-st.subheader(" Temperature Trend")
+st.subheader("Temperature Trend")
 st.line_chart(df["temperature"])
 
 df["day_index"] = range(len(df))
@@ -31,8 +30,8 @@ y = df["temperature"]
 model = LinearRegression()
 model.fit(X, y)
 
-future_day = np.array([[len(df)+1]])
+future_day = np.array([[len(df) + 1]])
 prediction = model.predict(future_day)[0]
 
-st.subheader(" Predicted Next Temperature")
+st.subheader("Predicted Next Temperature")
 st.success(f"{prediction:.2f} °C")
